fefd8413ecb78292a82a80172000daa7dfe0400b
Ivan_Dimov <IIDimov18@codingburgas.bg>
2020-01-14 22:53:35 +0200

    Added Google map

--- Web/Navigation_bar.js
+++ Web/Navigation_bar.js
@@ -1,21 +1,10 @@
 
-$("body").on('click', function (e) {
-    if (e.target.id !== 'sidebarCollapse"') {
-        Nav(0);
-    }
-});
+// $("body").on('click', function (e) {
+//     if (e.target.id !== 'sidebarCollapse"') {
+//         Nav(0);
+//     }
+// });
 
-function Nav(param) {
-    if (param === 0) {
+function Nav() {
         $('#sidebar, #content').toggleClass('active');
-        console.log("ekran");
-
-    } else {
-        $('#sidebar, #content').toggleClass('');
-        console.log("buton");
-    }
-}
-function NavButton(e) {
-    e.stopPropagation();
-    Nav(1);
 }
