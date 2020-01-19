0f741b34d56ce7ab960393fd8243b3eb1df8214f
Ivan_Dimov <IIDimov18@codingburgas.bg>
2020-01-19 22:38:24 +0200

    Added Cookies and started upgrading google map

--- Web/JS/Google_Map.js
+++ Web/JS/Google_Map.js
@@ -1,18 +1,76 @@
 
-let map;
+var map, infoWindow,Lat_Cookie,Lng_Cookie,count;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 42.502787, lng: 27.452733},
-        zoom: 8
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 6
     });
+    function CahngePosition(){ google.maps.event.addListener(map, 'click', function(event) {
+        var latitude = event.latLng.lat();
+        var longitude = event.latLng.lng();
+        var marker = new google.maps.Marker({
+            position:   {lat: latitude, lng: longitude},
+            map:map
+        });
+
+    });}
+    infoWindow = new google.maps.InfoWindow;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        alert("You must set your position, because your browser doesn't support geolocation or you haven't allowed us to know your location");
+        infoWindow.open(map);
+    }
 }
-var marker = new google.maps.Marker({
-    position:   {lat: 42.548326, lng: 27.407177},
-    title:"Hello World!"
-});
-marker.setMap(map);
-google.maps.event.addListener(marker, "click", function (event) {
-    var latitude = event.latLng.lat();
-    var longitude = event.latLng.lng();
-    console.log( latitude + ', ' + longitude );
-});
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user=getCookie("co");
+    if (user != "") {
+        initMap()
+    } else {
+        user = prompt("Please enter your name:","");
+        if (user != "" && user != null) {
+            setCookie("username", user, 30);
+        }
+    }
+}
