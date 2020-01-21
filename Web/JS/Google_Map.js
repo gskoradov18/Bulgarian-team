c3e2707e9fd4dc1d9498567611869a2cb5955fc2
Ivan_Dimov <IIDimov18@codingburgas.bg>
2020-01-21 20:25:28 +0200

    Fixed third-party cookies problem

--- Web/JS/Google_Map.js
+++ Web/JS/Google_Map.js
@@ -60,8 +60,8 @@ function setCookie(cname,cvalue,exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    document.cookie = cname + "=" + cvalue + "; SameSite=None" + expires + ";path=/";
+    document.cookie = cname + "=" + cvalue + "; SameSite=None" + expires + ";path=/";
 }
 function getCookie(cname) {
     var name = cname + "=";
