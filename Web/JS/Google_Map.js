9f1c7467c99d173bf10c8c1de44dc8906e66c295
Ivan_Dimov <IIDimov18@codingburgas.bg>
2020-02-03 21:29:16 +0200

    Finally fixed cookies, after years and decades of working it's finally done

--- Web/JS/Google_Map.js
+++ Web/JS/Google_Map.js
@@ -1,36 +1,56 @@
 
 var map, infoWindow,latitude,longitude, gMarker = null;
 function initMap() {
+
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
         zoom: 6
     });
-
     infoWindow = new google.maps.InfoWindow;
+    if (checkCookie()!==""){
+        var fixedPos=checkCookie();
+        console.log(fixedPos);
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
+        let pos1 = {
+            lat: parseFloat(fixedPos[0]),
+            lng: parseFloat(fixedPos[1])
+        };
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-        }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
-
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        alert("You must set your position, because your browser doesn't support geolocation or you haven't allowed us to know your location");
+        console.log(pos1);
+        infoWindow.setPosition(pos1);
+        alert("baca1");
+        infoWindow.setContent('Your Location.');
         infoWindow.open(map);
+        map.setCenter(pos1);
+        console.log("MAKO");
+    }
+    else {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                console.log(position);
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                console.log("MAKO3");
+                console.log(pos);
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Your Location.');
+                infoWindow.open(map);
+                map.setCenter(pos);
+            }, function () {
+                handleLocationError(true, infoWindow, map.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+
+        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+            infoWindow.setPosition(pos);
+            alert("You must set your position, because your browser doesn't support geolocation or you haven't allowed us to know your location");
+            infoWindow.open(map);
+        }
     }
 }
 function ChangePosition(marker_count){
@@ -60,9 +80,8 @@ function setCookie(cname,cvalue,exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ';SameSite=None; Secure' + expires + ';path=/';
-    console.log("123");
-    document.cookie = cname + "=" + cvalue + ';SameSite=None; Secure' + expires + ';path=/';
+    document.cookie = cname + "=" + cvalue + ';SameSite=None; ' + expires + ';path=/';
+    document.cookie = cname + "=" + cvalue + ';SameSite=None; ' + expires + ';path=/';
 }
 function getCookie(cname) {
     var name = cname + "=";
@@ -81,10 +100,12 @@ function getCookie(cname) {
 }
 
 function checkCookie() {
-    var user=getCookie("co");
-    if (user !== "") {
+    var user=getCookie("Co1");
+    var user2 = getCookie("Co2");
+    if (user !== "undefined"&& user2!=="undefined"&&user!==""&&user2!=="") {
         document.getElementById("MapButtonNot").style.visibility="hidden";
         document.getElementById("MapButton").style.visibility="hidden";
+        return [user, user2];
     } else {
             setCookie("Co1", latitude, 30);
             setCookie("Co2",longitude , 30);
