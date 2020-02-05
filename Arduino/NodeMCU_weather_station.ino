70ea336696df72a19679a4b0ebe8f99698b11f88
mvatanasov18 <54852701+mvatanasov18@users.noreply.github.com>
2020-02-05 23:13:57 +0200

    code with the wifi "things"
    hided ssid, pass, auth token
    added comments

---
+++ Arduino/NodeMCU_weather_station.ino
@@ -0,0 +1,130 @@
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "your-token";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "name_of_your_network";
+char pass[] = "password_for_your-network";
+
+char databuffer[36];
+double temp;
+char dataString[50] = {0};
+
+BlynkTimer timer;
+
+void getBuffer()                                                                    //Get weather status data
+{
+  int index;
+  for (index = 0;index < 35;index ++)
+  {
+    if(Serial.available())
+    {
+      databuffer[index] = Serial.read();
+      if (databuffer[0] != 'c')
+      {
+        index = -1;
+      }
+    }
+    else
+    {
+      index --;
+    }
+  }
+}
+
+int transCharToInt(char *_buffer,int _start,int _stop)                               //char to int）
+{
+  int _index;
+  int result = 0;
+  int num = _stop - _start + 1;
+  int _temp[num];
+  for (_index = _start;_index <= _stop;_index ++)
+  {
+    _temp[_index - _start] = _buffer[_index] - '0';
+    result = 10*result + _temp[_index - _start];
+  }
+  return result;
+}
+
+int WindDirection()                                                                  //Wind Direction
+{
+  return transCharToInt(databuffer,1,3);
+}
+
+float WindSpeedAverage()                                                             //air Speed (1 minute)
+{
+  temp = 0.44704 * transCharToInt(databuffer,5,7);
+  return temp;
+}
+
+float WindSpeedMax()                                                                 //Max air speed (5 minutes)
+{
+  temp = 0.44704 * transCharToInt(databuffer,9,11);
+  return temp;
+}
+
+float Temperature()                                                                  //Temperature ("C")
+{
+  temp = (transCharToInt(databuffer,13,15) - 32.00) * 5.00 / 9.00;
+  return temp;
+}
+
+float RainfallOneHour()                                                              //Rainfall (1 hour)
+{
+  temp = transCharToInt(databuffer,17,19) * 25.40 * 0.01;
+  return temp;
+}
+
+float RainfallOneDay()                                                               //Rainfall (24 hours)
+{
+  temp = transCharToInt(databuffer,21,23) * 25.40 * 0.01;
+  return temp;
+}
+
+int Humidity()                                                                       //Humidity
+{
+  return transCharToInt(databuffer,25,26);
+}
+
+float BarPressure()                                                                  //Barometric Pressure
+{
+  temp = transCharToInt(databuffer,28,32);
+  return temp / 10.00;
+}
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  getBuffer();
+  sprintf(dataString, "%0.3d,%.3f,%.3f,%.3f,%.3f,%0.3d,%.3f", WindDirection(), WindSpeedMax(), RainfallOneHour(), RainfallOneDay(), Temperature(), Humidity(), BarPressure());//making a string with the data from the station's sensors
+  Serial.println(dataString);//showing the string
+  Blynk.virtualWrite(V5, dataString);//sending data to blynk via virtual pin
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(2400);
+
+  Blynk.begin(auth, ssid, pass);
+
+  // Setup a function to be called every second
+  timer.setInterval(2000L, myTimerEvent);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run(); // Initiates BlynkTimer
+}
