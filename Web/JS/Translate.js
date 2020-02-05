b62c61af5706ff36fc71bbbe257bcbb7296c424a
gskoradov18 <58163119+gskoradov18@users.noreply.github.com>
2020-02-05 19:20:10 +0200

    finished translating the about page im en.

--- Web/JS/Translate.js
+++ Web/JS/Translate.js
@@ -51,7 +51,7 @@ var obj = {
     "Text3": {
         bg: `Морските ресурси, както крайбрежни, така и океански, осигуряват незаменими ползи и услуги, които съответстват на целите на близнаци на Групата на Световната банка 
         за премахване на крайната бедност и стимулиране на споделения просперитет. Въпреки това океаните са в криза: замърсяване на морето и пластмасови отпадъци, заедно с 
-        нарастващото въздействие на изменението на климата, застрашават морските екосистеми, здравето на крайбрежните общности и ресурсите и услугите, които водят до растеж. 
+        нарастващото въздействие на климатичните промени, застрашават морските екосистеми, здравето на крайбрежните общности и ресурсите и услугите, които водят до растеж. 
         Освен това нарастващото търсене на океански и сухопътни ресурси може да влоши съществуващото прекомерно използване на океанските ресурси и услуги, като същевременно 
         увеличава потреблението на пластмаса и потоците от отпадъци. Без съгласуван отговор тези заплахи ще изчерпат допълнително риболова (от които над 30 процента са свръхулов), 
         ще унищожат крайбрежните влажни зони и ще причинят пагубни ефекти върху природните богатства, които привличат туризма в крайбрежните общности. 
@@ -68,12 +68,19 @@ var obj = {
         ru: ""
     },
     "title1.1" : {
-        bg: "",
+        bg: "За качеството на въздуха.",
         en: "About the Air quality.",
         ru: ""
     },
     "text1.1" : {
-        bg: "",
+        bg: `Въпреки десетилетия напредък, качеството на въздуха в Съединените щати започна да намалява през последните няколко години, сочат данните, 
+        предоставени през лятото на 2019 г. от Агенцията за опазване на околната среда. Агенцията регистрира 15 процента повече дни с нездравословен въздух 
+        в страната през 2018 г. и 2017 г. в сравнение със средното 2013-2016.<br><br>Причините за скорошния спад на качеството на въздуха остават неясни,
+        казва агенцията, но може да са свързани с големия брой пожари, затоплящия климат и нарастващите модели на човешкото потребление, 
+        водени от растежа на населението и силната икономика. 
+        Дългосрочната перспектива също остава неясна, дори когато политиците обсъждат стандартите за замърсяване на въздуха.
+        `,
+
         en: `Despite decades of progress, the air quality in the United States has started to decline over the past few years, according to data provided in summer 2019 
                 by the Environmental Protection Agency. The agency recorded 15 percent more days with unhealthy air in the country in 2018 and 2017 compared to the average from 2013 
                 to 2016. <br><br> The reasons for the recent decline in air quality remain unclear, says the agency, but may be related to high numbers of wildfires, a warming climate, 
@@ -82,22 +89,41 @@ var obj = {
         ru: ""
     },
     "title1.2" : {
-        bg: "",
+        bg: "Какво е замърсяването на въздуха?",
         en: "What is air pollution?",
         ru: ""
     },
     "text1.2" : {
-        bg: "",
-        en: "",
+        bg: `Замърсяването на въздуха е смес от частици и газове, които могат да достигнат вредни концентрации както навън, така и на закрито. 
+        Неговите ефекти от него могат да варират от по-висок риск от заболяване до повишаване на температурата. 
+        Сажди, дим, плесен, прашец, метан и въглероден диоксид са само няколко примера за често срещани замърсители.
+        <br><br>В САЩ една мярка за замърсяване на въздуха на открито е индексът за качество на въздуха или AQI, 
+        който оценява условията на въздуха в цялата страна въз основа на концентрации на пет основни замърсители: 
+        озон на земята, замърсяване с частици (или прахови частици), въглероден оксид, серен диоксид и азотен диоксид. 
+        Някои от тях също допринасят за замърсяването на въздуха в помещението, заедно с радон, цигарен дим, летливи органични съединения (ЛОС), формалдехид, азбест и други вещества.`,
+
+        en:`Air pollution is a mix of particles and gases that can reach harmful concentrations both outside and indoors. 
+            Its effects can range from higher disease risks to rising temperatures. Soot, smoke, mold, pollen, methane, and carbon dioxide are a just few examples of common pollutants.
+            <br><br>
+            In the U.S., one measure of outdoor air pollution is the Air Quality Index, or AQI which rates air conditions 
+            across the country based on concentrations of five major pollutants: ground-level ozone, particle pollution (or particulate matter), carbon monoxide, 
+            sulfur dioxide, and nitrogen dioxide. 
+            Some of those also contribute to indoor air pollution, along with radon, cigarette smoke, volatile organic compounds (VOCs), formaldehyde, asbestos, and other substances.`,
         ru: ""
     },
     "title1.3" : {
-        bg: "",
+        bg: "Глобална опасност за здравето",
         en: "A global health hazard",
         ru: ""
     },
     "text1.3" : {
-        bg: "",
+        bg: `Лошото качество на въздуха убива хора. В световен мащаб лошият въздух на открито е причинил приблизително 4,2 милиона преждевременни смъртни случаи през 2016 г., 
+        като около 90 процента от тях са в страни с нисък и среден доход, според Световната здравна организация. 
+        Димът на закрито е продължаваща заплаха за здравето на 3-те милиарда души, които готвят и отопляват домовете си, като изгарят биомаса, керосин и въглища.
+        Замърсяването на въздуха е свързано с по-високите проценти на рак, сърдечни заболявания, инсулт и респираторни заболявания като астма.
+        <br><br>В САЩ близо 134 милиона души - над 40 процента от населението - са изложени на риск от заболяване и преждевременна смърт поради замърсяване на въздуха, 
+        според оценките на Американската асоциация на белия дроб.`,
+
         en: `Poor air quality kills people. Worldwide, bad outdoor air caused an estimated 4.2 million premature deaths in 2016, about 90 percent of them in low- and middle-income
          countries, according to the World Health Organization. Indoor smoke is an ongoing health threat to the 3 billion people who cook and heat their homes by burning biomass, 
          kerosene, and coal. Air pollution has been linked to higher rates of cancer, heart disease, stroke, and respiratory diseases such as asthma.  
@@ -106,79 +132,94 @@ var obj = {
         ru: ""
     },
     "title1.4" : {
-        bg: "",
+        bg: "СНИМКИ НА ВЪЗДЕЙСТВИЯТА НА ГЛОБАЛНОТО ЗАТОПЛЯНЕ",
         en: "PHOTOS OF GLOBAL WARMING IMPACTS",
         ru: ""
     },
     "img1txt" : {
-        bg: "",
+        bg: "Айсберг се топи във водите край Антарктида. Климатичните промени са ускорили загубата на лед в целия континент.",
         en: "An iceberg melts in the waters off Antarctica. Climate change has accelerated the rate of ice loss across the continent.",
         ru: ""
     },
     "img2txt" : {
-        bg: "",
+        bg: "С повишаването на морското равнище солените океански води влизат в Евърглейдс на Флорида. Местни растения и животни се мъчат да се приспособят към променящите се условия.",
         en: "As sea levels rise, salty ocean waters encroach into Florida’s Everglades. Native plants and animals struggle to adapt to the changing conditions.",
         ru: ""
     },
 
     "img3txt" : {
-        bg: "",
+        bg: "Западният САЩ от години е затворен в суша. Сухото, горещо време е увеличило интензивността и разрушителността на горските пожари.",
         en: "The western U.S. has been locked in a drought for years. The dry, hot weather has increased the intensity and destructiveness of forest fires.",
         ru: ""
     },
     "img4txt" : {
-        bg: "",
+        bg: `Древните гори около тропиците се изсичат, за да се направи място за плантации с палмово масло. Когато тези гори се загубят, въглеродът, 
+        който те държат заключен в тъканите си, се отделя в атмосферата, допринасяйки за по-нататъшно глобално затопляне.`,
+
         en: `Ancient forests around the tropics are being cut down to make space for palm oil plantations. When these forests are lost, the carbon they held locked up in their 
         tissues is released into the atmosphere, contributing to further global warming.`,
         ru: ""
     },
     "img5txt" : {
-        bg: "",
+        bg: `Във високите равнини на Боливия човек изследва изпечените останки от второто по големина езеро в страната - езерото Пуопо. 
+        Проблемите със сушата и управлението предизвикаха пресъхването на езерото.`,
         en: `In the high plains of Bolivia, a man surveys the baked remains of what was the country’s second largest lake, Lake Poopó. Drought and management issues have caused 
         the lake to dry up.`,
         ru: ""
     },
     "img6txt" : {
-        bg: "",
+        bg: `Климатичните промени оказват влияние върху флората и фауната в Арктика. Въпреки че учените не знаят конкретно какво е убило тази индивид полярна мечка, 
+        експертите предупреждават, че много от мечките имат проблеми с намирането на храна като морския лед, на който исторически са разчитали се изтънява и се стопява по-рано.`,
+        
         en: `Climate change is impacting flora and fauna across the Arctic. Although scientists don't know specifically what killed this individual polar bear, experts warn that 
         many of the bears are having trouble finding food as the sea ice they historically relied on thins and melts earlier.`,
         ru: ""
     },
     "img7txt" : {
-        bg: "",
+        bg: `Езерото Урмия, в Иран, е критично местообитание на птиците и се използва като популярна туристическа дестинация. 
+        То пресъхва поради климатичните промени и проблеми с управлението.`,
         en: "Lake Urmia, in Iran, is a critical bird habitat and used to be a popular tourist destination. It is drying up because of climate change and management issues.",
         ru: ""
     },
     "img8txt" : {
-        bg: "",
+        bg: `Електроцентралата Шерер в Жулиета, Джорджия, е най-голямата електроцентрала на въглища в САЩ. 
+        Тя изгаря 34 000 тона въглища дневно, изхвърляйки над 25 милиона тона въглероден диоксид в атмосферата всяка година.`,
         en: `The Scherer power plant in Juliet, Georgia, is the largest coal-fired power plant in the U.S. It burns 34,000 tons of coal daily, pumping over 25 million tons of 
         carbon dioxide into the atmosphere each year.`,
         ru: ""
     },
     "img9txt" : {
-        bg: "",
+        bg: `Лед се топи върху планинско езеро. 
+        Езерата по света замръзват все по-малко с течение на времето и след няколко десетилетия хиляди езера по света може да загубят изцяло зимната си ледена покривка.`,
         en: `Ice melts on a mountain lake. Lakes around the world are freezing less and less over time, and in a few decades, thousands of lakes around the world may lose their 
         winter ice cover entirely.`,
         ru: ""
     },
     "img10txt" : {
-        bg: "",
+        bg: `Амазонка губи еквивалент на близо един милион футболни игрища от горскa покривка всяка година, голяма част от които се отсичат, за да се направи път за селското стопанство. 
+        Когато се загуби гората, отделеният от нея въглерод се озовава в атмосферата, ускорявайки климатичните промени.`,
+        
         en: `The Amazon is losing the equivalent of nearly one million soccer fields of forest cover each year, much of which is cut down to make way for agriculture. When forest is 
         lost, the carbon it sequestered ends up in the atmosphere, accelerating climate change.`,
         ru: ""
     },
     "img11txt" : {
-        bg: "",
+        bg: "В Националния парк Ледник горите усещат ефектите от ранното топене на снега и дългите, сухи лета. Стресът върху флората на парка се усилва от климатичните промени.",
         en: "In Glacier National Park, forests are feeling the effects of early snowmelt and long, dry summers. The stresses on the park's flora are exacterbated by climate change.",
         ru: ""
     },
     "img12txt" : {
-        bg: ``,
+        bg: `Парниковите газове са ключов фактор за променящия се климат на Земята.`,
         en: `Greenhouse gases are a key factor in the Earth's changing climate.`,
         ru: ""
     },
     "text1.4" : {
-        bg: ``,
+        bg: `Замърсителите на въздуха причиняват по-малко директни последици за здравето, когато допринасят за изменението на климата. 
+        Топлинните вълни, екстремното време, прекъсването на доставките на храни и други ефекти, свързани с повишените парникови газове, 
+        могат да имат отрицателно въздействие върху човешкото здраве. Американската четвърта национална оценка на климата, 
+        публикувана през 2018 г., отбеляза например, че променящият се климат 
+        „може да изложи повече хора в Северна Америка на кърлежи, които носят лаймска болест и комари, които предават вируси като Западен Нил, чикунгуня, денга и Зика“.`,
+
         en: `Air pollutants cause less-direct health effects when they contribute to climate change. Heat waves, extreme weather, food supply disruptions, 
         and other effects related to increased greenhouse gases can have negative impacts on human health. 
         The U.S. Fourth National Climate Assessment released in 2018 noted, for example, that a changing climate "could expose more people in 
@@ -186,12 +227,16 @@ var obj = {
         ru: ``
     },
     "title1.5" : {
-        bg: ``,
+        bg: `Въздействията върху околната среда`,
         en: `Environmental impacts`,
         ru: ``
     },
     "text1.5" : {
-        bg: ``,
+        bg: `Въпреки че много живи същества отделят въглероден диоксид, когато дишат, газът се счита за замърсител, 
+        когато се свързва с автомобили, самолети, електроцентрали и други човешки дейности, които включват изгаряне на изкопаеми горива като бензин и природен газ. 
+        Това е така, защото въглеродният диоксид е най-често срещаният от парниковите газове, които улавят топлината в атмосферата и допринасят за изменението на климата. 
+        Хората са изпомпвали достатъчно въглероден диоксид в атмосферата през последните 150 години, за да повишат нивата му по-високи, отколкото са били стотици хиляди години.`,
+
         en: `Though many living things emit carbon dioxide when they breathe, the gas is widely considered to be a pollutant when associated with cars, planes, power plants,
         and other human activities that involve the burning of fossil fuels such as gasoline and natural gas.
         That's because carbon dioxide is the most common of the greenhouse gases, which trap heat in the atmosphere and contribute to climate change. 
@@ -199,24 +244,45 @@ var obj = {
         ru: ``
     },
     "text1.6" : {
-        bg: ``,
+        bg: `Друг замърсител, свързан с изменението на климата, е серен диоксид, компонент на смог. 
+        Серен диоксид и тясно свързани химикали са известни предимно като причина за киселинен дъжд. 
+        Но те също отразяват светлината, когато се отделят в атмосферата, което задържа слънчевата светлина навън и създава охлаждащ ефект. 
+        Вулканичните изригвания могат да отделят огромни количества серен диоксид в атмосферата, понякога причинявайки охлаждане, което продължава с години. 
+        Всъщност вулканите са били основният източник на атмосферен серен диоксид; Днес, хората са.<br><br>
+        Въздушните частици, в зависимост от химичния им състав, могат да имат и директни ефекти, отделни от климатичните промени. 
+        Те могат да променят или изчерпват хранителните вещества в почвата и водните пътища, да навредят на горите и културите и да повредят културни икони като паметници и статуи.`,
+
         en: `Another pollutant associated with climate change is sulfur dioxide, a component of smog. Sulfur dioxide and closely related chemicals are known primarily 
         as a cause of acid rain.
         But they also reflect light when released in the atmosphere, which keeps sunlight out and creates a cooling effect. 
         Volcanic eruptions can spew massive amounts of sulfur dioxide into the atmosphere, sometimes causing cooling that lasts for years. 
-        In fact, volcanoes used to be the main source of atmospheric sulfur dioxide; today, people are.
+        In fact, volcanoes used to be the main source of atmospheric sulfur dioxide; Today, people are.
 
         <br><br>Airborne particles, depending on their chemical makeup, can also have direct effects separate from climate change. 
         They can change or deplete nutrients in soil and waterways, harm forests and crops, and damage cultural icons such as monuments and statues.`,
         ru: ``
     },
     "title1.7" : {
-        bg: ``,
+        bg: `Какво може да се направи?`,
         en: `What can be done?`,
         ru: ``
     },
     "text1.7" : {
-        bg: ``,
+        bg: `Страните по света се справят с различни форми на замърсяване на въздуха. 
+        Китай например постига напредък в почистването на задушено небе от години на бърза индустриална експанзия, отчасти чрез затваряне или анулиране на електроцентрали, 
+        работещи с въглища. В САЩ Калифорния е лидер в определянето на стандарти за вредни емисии, насочени към подобряване на качеството на въздуха, 
+        особено на места като известния мътен Лос Анджелис. 
+        И най-различни усилия имат за цел да приведат по-чисти възможности за готвене на места, където преобладават опасните готварски печки.
+
+        <br><br>Във всеки дом хората могат да се предпазят от замърсяване на въздуха в затворени помещения чрез увеличаване на вентилацията, 
+        тестване на газ от радон, използване на пречистватели на въздуха, работещи вентилатори за кухня и баня и избягване на пушенето. 
+        Когато работите върху домашни проекти, потърсете боя и други продукти с ниско съдържание на летливи органични съединения: 
+        организации като Green Seal, UL (GREENGUARD) и Съветът за зелено строителство на САЩ могат да помогнат.
+
+        <br><br>За да се ограничи глобалното затопляне, трябва да се предприемат различни мерки, като например добавяне на повече възобновяема енергия и замяна на автомобили 
+        с бензин с автомобили с нулеви емисии, като електрическите. В по-голям мащаб правителствата на всички нива поемат ангажименти за ограничаване на емисиите на
+        въглероден диоксид и други парникови газове. Парижкото споразумение, ратифицирано на 4 ноември 2016 г., е едно от усилията за борба с изменението на климата в глобален мащаб. 
+        Поправката от Кигали се стреми да подобри напредъка, постигнат от Монреалския протокол, като забранява въглеводородните въглеводороди (HFC) в допълнение към CFC.`,
 
         en: `Countries around the world are tackling various forms of air pollution. China, for example, is making strides in cleaning up smog-choked skies 
         from years of rapid industrial expansion, partly by closing or canceling coal-fired power plants. 
